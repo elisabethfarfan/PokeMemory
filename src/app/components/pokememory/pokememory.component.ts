4536254c223866fff9ca1e39fdcbea897709b954
joselyncondoricabrera <condori.cabrera.joselyn@gmail.com>
2023-02-06 23:48:14 -0500

    mostrar matriz de pokemones y css

--- src/app/components/pokememory/pokememory.component.ts
+++ src/app/components/pokememory/pokememory.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import cardsjson from '../../../../data/pokemon/pokemon.json';
+
+interface pokemon {
+  id: string,
+  image: string,
+  bgColor: string
+}
 
 @Component({
   selector: 'app-pokememory',
   templateUrl: './pokememory.component.html',
   styleUrls: ['./pokememory.component.css']
 })
+
 export class PokememoryComponent {
+  pokeData : pokemon[] = cardsjson.items;
+
+  constructor(){
+    console.log(cardsjson.items);
+  }
 
 }
