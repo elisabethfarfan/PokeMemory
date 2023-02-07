6dea004181a99cdd2952e9bd4e94d0b6b867345e
joselyncondoricabrera <condori.cabrera.joselyn@gmail.com>
2023-02-07 09:35:29 -0500

    juntando cambios css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,10 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { PreviaCardsComponent } from './components/previa-cards/previa-cards.component';
-<<<<<<< HEAD
 import { PokedexComponent } from './components/pokedex/pokedex.component';
 import { HomeComponent } from './components/home/home.component';
 import { PokememoryComponent } from './components/pokememory/pokememory.component';
-=======
 import { FooterComponent } from './components/footer/footer.component';
-import { PokeMemoryComponent } from './components/poke-memory/poke-memory.component';
->>>>>>> 9f0efe8161b547cbe49af92d710f0aa2329c611b
 
 @NgModule({
   declarations: [
@@ -21,14 +17,10 @@ import { PokeMemoryComponent } from './components/poke-memory/poke-memory.compon
     NavbarComponent,
     SliderComponent,
     PreviaCardsComponent,
-<<<<<<< HEAD
     PokedexComponent,
     HomeComponent,
-    PokememoryComponent
-=======
+    PokememoryComponent,
     FooterComponent,
-    PokeMemoryComponent
->>>>>>> 9f0efe8161b547cbe49af92d710f0aa2329c611b
   ],
   imports: [
     BrowserModule,
