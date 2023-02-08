0343d3b359ea941fa25daec82bbc009da138608a
joselyncondoricabrera <condori.cabrera.joselyn@gmail.com>
2023-02-08 12:19:39 -0500

    datos obtenido de la pokeapi en consola

--- src/app/components/pokememory/pokememory.component.ts
+++ src/app/components/pokememory/pokememory.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import cardsjson from '../../../../data/pokemon/pokemon.json';
 
 interface pokemon {
@@ -15,11 +15,25 @@ interface pokemon {
 
 export class PokememoryComponent {
   pokeData : pokemon[] = cardsjson.items;
+  eventClick: boolean = false;
+  nameDiv ?: string | undefined = '';
 
-  constructor(){
+  @ViewChild('cardpokemon2') myCard!: ElementRef;
+
+
+  // voltearCard(i:number){
+  //   console.log("click",i);
+  //   this.eventClick = true;
+  //   this.nameDiv =`cardpokemon${i}`;
+  // }
+  changeCard(){
+    const div = this.myCard.nativeElement;
+    console.log(div);
+  }
+
+  constructor( private renderer2 : Renderer2 ){
     this.pokeData = [...this.pokeData,...this.pokeData];
     this.pokeData.sort( function(){ return Math.random() - 0.5});
-    console.log(Math.random());
   }
 
 }
