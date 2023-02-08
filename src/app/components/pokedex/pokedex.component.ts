9ea3be990cb9873d0d7e89a9eb61526645faa168
elisabethfarfan <elisabethfarfan@gmail.com>
2023-02-08 12:16:35 -0500

    try card

--- src/app/components/pokedex/pokedex.component.ts
+++ src/app/components/pokedex/pokedex.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
+import { of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pokedex',
   templateUrl: './pokedex.component.html',
   styleUrls: ['./pokedex.component.css']
 })
-export class PokedexComponent {
 
+
+export class PokedexComponent { 
+
+   pokemons: any =[];
+
+  constructor(){
+    this.getPokemons();
+  }
+  
+
+  getPokemons(){
+
+    axios.get(' https://pokeapi.co/api/v2/pokemon')
+    .then((e:any) => {
+      this.pokemons.push(e.data.results);
+    } 
+      )
+
+      const squareValues = this.pokemons.map((val: any) => val.url);
+      console.log(squareValues);
+   
+  }
 }
